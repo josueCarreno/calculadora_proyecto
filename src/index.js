455039f3b7dce27dab11fa754d1cb3776a5d1da3
josueCarreno <150408018+josueCarreno@users.noreply.github.com>
2025-05-31 22:44:46 -0400

    validacion de entrada de datos

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 document.querySelectorAll('.boton').forEach(boton => {
         boton.addEventListener('click', function() {
-          
-          let entrada = document.calculadora.entrada.value + this.value;
-          let entrada_regex = /^[0-9]+$/;
-
-          //if(entrada_regex.test(entrada) ) {
-            document.calculadora.entrada.value  += this.value;
-          //}
-        
+          document.calculadora.entrada.value  += this.value;
         });
     });
 
 document.getElementById('resultado').addEventListener('click', function() {
   let nuevoValor = document.calculadora.entrada.value.replaceAll("x", "*");
   nuevoValor = nuevoValor.replaceAll("^", "**");
-  resultado = eval(nuevoValor);
-  
-  document.calculadora.entrada.value = resultado
+  nuevoValor = nuevoValor.replaceAll("÷", "/");
+  //resultado = eval(nuevoValor);
+
+  try {
+    let regexOperation = /[+\-*/%.0-9()]/g;
+    if (!regexOperation.test(nuevoValor)) {
+      throw new Error("Operación inválida");
+    }
+    const resultado = eval(nuevoValor);
+    if (resultado === Infinity || resultado === -Infinity) {
+      nuevoValor = "";
+      throw new Error("División entre Cero");
+    }
+    document.calculadora.entrada.value = resultado;
+  } catch (error) {
+    alert("Error en la calculación: " + error.message);
+    document.calculadora.reset();
+  }
+
 });
 
 document.getElementById('borrar').addEventListener('click', function() {
